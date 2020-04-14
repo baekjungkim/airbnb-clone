4b3db522220cd612059bf8e9d9b7fcafbb542fd9
baekjung <zvgandam@gmail.com>
2020-04-14 10:12:42 +0900

    Review Admin and Room Average

--- reviews/models.py
+++ reviews/models.py
@@ -22,3 +22,18 @@ class Review(core_models.TimeStampedModel):
 
     def __str__(self):
         return f"{self.user} : {self.review} - {self.room}"
+
+    def rating_average(self):
+        avg = (
+            self.accuracy
+            + self.communication
+            + self.cleanliness
+            + self.location
+            + self.check_in
+            + self.value
+        ) / 6
+
+        # 1자리수까지 반올림
+        return round(avg, 1)
+
+    rating_average.short_description = "AVG."
