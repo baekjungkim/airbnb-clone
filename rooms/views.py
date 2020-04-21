5bb44f44537968b780ed46d55044f57a6fff45bb
baekjung <zvgandam@gmail.com>
2020-04-21 16:02:05 +0900

    Finishing the Form

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 
@@ -22,6 +23,48 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = request.GET.get("city")
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city": city})
+    s_country = request.GET.get("country", "KR")
+    s_room_type = int(request.GET.get("room_type", 0))
+    price = request.GET.get("price", 0)
+    guest = request.GET.get("guest", 0)
+    bedrooms = request.GET.get("bedrooms", 0)
+    beds = request.GET.get("beds", 0)
+    baths = request.GET.get("baths", 0)
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+    s_house_rules = request.GET.getlist("house_rules")
+
+    form = {
+        "city": city,
+        "s_country": s_country,
+        "s_room_type": s_room_type,
+        "price": price,
+        "guest": guest,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "instant": instant,
+        "super_host": super_host,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "s_house_rules": s_house_rules,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+    house_rules = models.HouseRule.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+        "house_rules": house_rules,
+    }
+
+    return render(request, "rooms/search.html", {**form, **choices},)
