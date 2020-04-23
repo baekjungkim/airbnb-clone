dfb44ed8ac31bf9e4b162e92c0441cc1c376fbe1
baekjung <zvgandam@gmail.com>
2020-04-23 12:23:30 +0900

    Login and Logout

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from rooms import views as room_views
+from users import views as user_views
 
 app_name = "core"
 
-urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]
+urlpatterns = [
+    path("", room_views.HomeView.as_view(), name="home"),
+    path("login", user_views.LoginView.as_view(), name="login"),
+    path("logout", user_views.log_out, name="logout"),
+]
